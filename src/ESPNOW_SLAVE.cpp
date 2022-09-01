f7eec5687d080a2bcc992e3585210ba512114d7d
nezumimrx <nezumimrx@gmail.com>
2022-09-01 17:11:21 +0800

    编程模式自动切换为慢速

--- src/ESPNOW_SLAVE.cpp
+++ src/ESPNOW_SLAVE.cpp
@@ -65,6 +65,9 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 
     }else if(*received_data.x=='0'){
         if(received_data.y==0)connected_with_controller=true;
+    }else if(*received_data.x=='s'){
+        if(received_data.y==0)motor_speed=255;
+        else if(received_data.y==1)motor_speed=125;
     }
 
 }
